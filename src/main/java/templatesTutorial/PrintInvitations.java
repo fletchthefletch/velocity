72e28c3fd1d3ae28c1b75cb03ee4001e6734d581
fletchthefletch <fletcher@4tradeit.co.nz>
2021-09-25 14:21:24 +1200

    Reads from invitation template

--- src/main/java/templatesTutorial/PrintInvitations.java
+++ src/main/java/templatesTutorial/PrintInvitations.java
@@ -1,9 +1,17 @@
 package templatesTutorial;
 
+import java.awt.List;
 import java.beans.XMLDecoder;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 public class PrintInvitations {
 	
@@ -11,9 +19,9 @@ public class PrintInvitations {
 	public static void main(String[] args) {
 
 		// XMl Decoder
-       XMLDecoder d = null;
+       XMLDecoder decoder = null;
 		try {
-			d = new XMLDecoder(
+			decoder = new XMLDecoder(
 			           new BufferedInputStream(
 			               new FileInputStream("friends.xml")));
 		} catch (FileNotFoundException e) {
@@ -21,11 +29,57 @@ public class PrintInvitations {
 			e.printStackTrace();
 		}
 		
+		try {
+			// Get XML parent object
+			Object allObjects = decoder.readObject();
+			decoder.close();
+
+			
+			// Get children of object
+			ArrayList<Person> persons = (ArrayList<Person>) allObjects;
+			
+			for (Person person : persons)
+			{
+				System.out.println(person.getFirstName());
+
+				
+				VelocityContext context = new VelocityContext();
+				context.put( "person", person);
+
+				
+				
+				Template template = Velocity.getTemplate("invitation.vm");
+				
+				// Write to an invitations file, that shall contain all invitations
+				FileWriter out = null;
+				try {
+					out = new FileWriter("invitations.txt");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				template.merge(context, out);
+				
+				try {
+					out.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				
+				
+				
+				
+			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 		
-		Object result = d.readObject();
-		d.close();
-		
-		System.out.println(d.toString());
+			
 	}
 
 }
